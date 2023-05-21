1c9b8f142c4574dc124069b0e4fe83e749e6c1c7
Eric <esbev@hotmail.com>
2023-05-20 20:34:14 -0500

    cotinued working on js code

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -17,31 +17,37 @@ var questionIndex = 0;
 var questionTextArray = [
   "why did the chicken cross the road? Answer: To get to ___________.",
   "What is your favorite color?",
-  "How many hours are in a day?",];
+  "How many hours are in a day?",
+  "The answer is A"];
 var choice1Text = [
   "1: the food",
   "Blue",
-  "24"];
+  "24",
+  "4"];
 var choice2Text = [
   "2: the water",
   "Red",
-  "16"];
+  "16",
+  "3"];
 var choice3Text = [
   "3: the otherside",
   "Yellow",
-  "36"];
+  "36",
+  "2"];
 var choice4Text = [
   "4: Alpha Centauri",
   "Flourescent Orange",
-  "72"];
-var correctAnswerIndex = [3,4,1];
+  "72",
+  "1"];
+var correctAnswerIndex = [3,4,1,4];
 
-var timeLeft = 15;
-var timerSpeed = 500;
+var userScore = "";
+var timeLeft = 40;
+var timerSpeed = 1000;
 var clickCount = 0;
 var correctAnswers = 0;
-var totalQuestions = 3;
 var answerString = "";
+var resultText = "";
 
 function init(){
   //sets initial view
@@ -61,11 +67,11 @@ function startGame() {
 
         if (!timeLeft) {
             clearInterval(gameInterval);
-            //changes view on timer finish
+            //changes view when timer finish
             quizView.setAttribute("style", "display: none;");
             endView.setAttribute("style", "display: visible;");
-            
-            scoreText.textContent = Math.round(correctAnswers/totalQuestions*100) + "%";
+            userScore = toString(Math.round(correctAnswers/questionTextArray.length*100) + "%");
+            scoreText.textContent = userScore;
         }
     }, timerSpeed)
         timerSpan.textContent = timeLeft;
@@ -79,15 +85,18 @@ function saveHighScore () {
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
     var newScore = {
-      theScore: time,
+      theScore: userScore,
       theInitials: theInitials,
     };
 
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
-
     window.location.href = "highscore.html";
   }
+}
+
+function clearHighScores() {
+  
 }
 
 function setQuestionText(){
@@ -96,28 +105,24 @@ function setQuestionText(){
   choiceButton2.textContent = choice2Text[questionIndex];
   choiceButton3.textContent = choice3Text[questionIndex];
   choiceButton4.textContent = choice4Text[questionIndex];
-  questionResultText.textContent = "";
+  questionResultText.textContent = resultText;
 }
 
 function checkAnswer(x) {
-  if (questionIndex == totalQuestions-1) {
-    questionText.textContent = "You have finished the quiz.";
-    choiceButton1.setAttribute("style", "display: none;");
-    choiceButton2.setAttribute("style", "display: none;");
-    choiceButton3.setAttribute("style", "display: none;");
-    choiceButton4.setAttribute("style", "display: none;");
-    questionResultText.textContent = "No more questions.";
-  } else {
     if (x == correctAnswerIndex[questionIndex]) {
       correctAnswers++;
-      questionResultText.textContent = "Correct!";
+      resultText = "Correct!";
     } else {
-      questionResultText.textContent = "Incorrect!";
+      resultText = "Incorrect!";
+      timeLeft = timeLeft - 10;
     }
-    questionIndex++;
-    setQuestionText();
-  }
-
+    if (questionIndex == questionTextArray.length-1) {
+      endView.setAttribute("style", "display: none;");
+      timeLeft = 1;
+    } else {
+      questionIndex++;
+      setQuestionText();
+    }
 }
 
 init();
